29b10282d20009455f37e00f46cc9fada8cff2ed
Olimpius-8 <lampochka.idey.8@gmail.com>
2020-10-14 10:08:53 +0300

    first step - write ConsoleHelper

--- ConsoleHelper.java
+++ ConsoleHelper.java
@@ -1,4 +1,35 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message){
+        System.out.println(message);
+    }
+
+    public static String readString(){
+        String message;
+        try{
+            message = reader.readLine();
+        }catch (IOException e){
+            System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            return readString();
+        }
+        return message;
+    }
+
+    public static int readInt(){
+        int number;
+        try{
+            number = Integer.parseInt(readString());
+        }catch (NumberFormatException e){
+            System.out.println("Произошла ошибка при попытке ввода числа. Повторите ввод еще раз.");
+            return readInt();
+        }
+        return number;
+    }
 }
