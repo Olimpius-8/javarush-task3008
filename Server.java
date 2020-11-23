f0847047caea09405c726cca2a48ecdf35d4d3e5
Olimpius-8 <lampochka.idey.8@gmail.com>
2020-11-23 10:24:31 +0300

    step 7 - realize send message all users

--- Server.java
+++ Server.java
@@ -1,21 +1,31 @@
 package com.javarush.task.task30.task3008;
 
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
     private static class Handler extends Thread {
         private Socket socket;
 
         public Handler(Socket socket){
             this.socket = socket;
         }
-
-
     }
 
+    public static void sendBroadcastMessage(Message message){
+        try{
+            for (Map.Entry<String, Connection> pair : connectionMap.entrySet())
+                pair.getValue().send(message);
+        }catch (IOException e){
+            System.out.println("Message is not send");
+        }
+
+    }
 
     public static void main(String args[]) throws IOException{
         ConsoleHelper helper = new ConsoleHelper();
