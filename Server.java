27a80330800a4503238c1cb6e478536b1bf94e8e
Olimpius-8 <lampochka.idey.8@gmail.com>
2020-11-23 09:52:40 +0300

    step 6 - realize loop on server

--- Server.java
+++ Server.java
@@ -1,7 +1,35 @@
 package com.javarush.task.task30.task3008;
 
-public class Server {
-    public static void main(String args[]){
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket){
+            this.socket = socket;
+        }
+
+
+    }
+
+
+    public static void main(String args[]) throws IOException{
+        ConsoleHelper helper = new ConsoleHelper();
+        try (ServerSocket serverSocket = new ServerSocket(helper.readInt())){
+            System.out.println("Server is online");
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                Handler handler = new Handler(clientSocket);
+                handler.start();
+                clientSocket.close();
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
