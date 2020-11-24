87f4df7867cc549724d63fa66e59ef84ad5af636
Olimpius-8 <lampochka.idey.8@gmail.com>
2020-11-24 10:35:37 +0300

    step 13 - add protected methods in client

--- client/Client.java
+++ client/Client.java
@@ -1,11 +1,52 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
 
+    protected String getServerAddress(){
+        ConsoleHelper.writeMessage("Enter server address:");
+        String server;
+        //do {
+            server = ConsoleHelper.readString();
+        //} while (!server.matches("^(localhost|([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\\.([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\\.([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\\.([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5]))$"));
+        return server;
+    }
+
+    protected int getServerPort(){
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName(){
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole(){
+        return true;
+    }
+
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        }catch (IOException e){
+            ConsoleHelper.writeMessage("Input error occured");
+            clientConnected = false;
+        }
+    }
 
     public class SocketThread extends Thread{
 
