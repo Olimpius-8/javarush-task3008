820bf06e438280e18a28a422f97a809f135b6431
Olimpius-8 <lampochka.idey.8@gmail.com>
2020-12-09 16:02:26 +0300

    step 20 : create gui

---
+++ client/ClientGuiModel.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task30.task3008.client;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class ClientGuiModel {
+    private final Set <String> allUserNames = new TreeSet<String>();
+    private String newMessage;
+
+    public Set<String> getAllUserNames() {
+        return Collections.unmodifiableSet(allUserNames);
+    }
+
+    public String getNewMessage() {
+        return newMessage;
+    }
+
+    public void setNewMessage(String newMessage) {
+        this.newMessage = newMessage;
+    }
+
+    public void addUser(String user){
+        allUserNames.add(user);
+    }
+
+    public void deleteUser(String user){
+        allUserNames.remove(user);
+    }
+}
